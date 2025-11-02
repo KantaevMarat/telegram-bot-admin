45e0c1a567a8dcd9d24dc74987ce67db0d0df568
Marat <marathaker007@gmail.com>
2025-11-03 01:38:45 +0300

    Add separate data-source.ts for migrations

---
+++ backend/dist/src/data-source.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const typeorm_1 = require("typeorm");
+exports.default = new typeorm_1.DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST || 'postgres',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USER || 'telegram_bot_user',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'telegram_bot_db',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
+    synchronize: false,
+});
+//# sourceMappingURL=data-source.js.map
---
+++ backend/src/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'postgres',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER || 'telegram_bot_user',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'telegram_bot_db',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
+
